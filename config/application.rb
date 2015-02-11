71b5efc765d52b2333d0ed2afdae5c99bf2b12ce
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-10 23:11:06 -0200

    pt-BR

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,10 @@ module Confiemim
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.default_locale = :"pt-BR"
-    I18n.locale = :"pt-BR"
+    I18n.enforce_available_locales = true
+    config.i18n.locale = :'pt-BR'
+    config.i18n.default_locale = :'pt-BR'
+
+    config.i18n.fallbacks = [:'pt-BR']
   end
 end
